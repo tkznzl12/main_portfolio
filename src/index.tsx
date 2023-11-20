095943e954606fb76a8631c7396075569ae515e9
tkznzl12 <tkznzl12@naver.com>
2023-11-21 01:28:42 +0900

    feat: react/clinent 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Reset } from "styled-reset";
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Reset />
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
