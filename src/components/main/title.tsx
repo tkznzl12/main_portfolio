0a0c895c9e82a680411c8d6a96c6d4f6ac69335a
tkznzl12 <tkznzl12@naver.com>
2023-11-21 01:45:30 +0900

    fix: 초기 실행시 한칸씩 건너띄워져서 출력되는 에러 수정

--- src/components/main/title.tsx
+++ src/components/main/title.tsx
@@ -37,29 +37,27 @@ const Title = () => {
   const text = "안녕하세요!\n 프론트엔드 개발자 \n정수진입니다.  ";
   const [remove, setRemove] = useState(false);
   useInterval(() => {
-    // remove함수
+    //1.setLendingText에 text글자가 한글자씩 추가되는 코드 작성
     if (typingNum > 0 && remove) {
       setLandingText((content) => {
         let result = content.slice(0, typingNum ? typingNum - 1 : 0);
-
         setTypingNum((content: number) => content - 1);
         if (typingNum - 1 === 0) {
           setRemove(false);
         }
         return result;
       });
-      //글자를 기록할때의 함수
     } else {
       setLandingText((content) => {
         let result = content ? content + text[typingNum] : text[0];
-
-        if (typingNum < text.length - 1) {
-          setTypingNum((content: number) => content + 1);
-        } else {
-          setRemove(true);
-        }
         return result;
       });
+
+      if (typingNum < text.length - 1) {
+        setTypingNum((content: number) => content + 1);
+      } else {
+        setRemove(true);
+      }
     }
   }, 150);
 
